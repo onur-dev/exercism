15475bb69488b9bb665eb85a39b55b9069da7170
ONUR DEMIRTAS <e1941939@ceng.metu.edu.tr>
2018-07-14 10:08:06 +0300

    beer-song refactor

--- beer-song/beer-song.js
+++ beer-song/beer-song.js
@@ -1,26 +1,39 @@
-const cases = {
-    0: "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n",
-    1: "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n",
-    2: "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n",
-};
-
 var BeerSong = function() {};
 
-BeerSong.prototype.verse = function(val) {
-    if(isNaN(val)) throw Error('invalid input!');
-    let currentVerse = val.toString(), nextVerse = (val - 1).toString();
-	var normalLine = `${val} bottles of beer on the wall, ${val} bottles of beer.\nTake one down and pass it around, ${val-1} bottles of beer on the wall.\n`;
-	return val in cases ? cases[val] : normalLine;
+BeerSong.prototype.verse = function(bottleCount) {
+ if(isNaN(bottleCount)) throw Error('invalid input!');
+ return this.leftPart(bottleCount) + '\n' + this.rightPart(bottleCount);
 };
 
+BeerSong.prototype.countPhrase = function(bottleCount) {
+ if (bottleCount === 0) return 'no more bottles';
+ if (bottleCount === 1) return '1 bottle';
+ return bottleCount.toString() + ' bottles';
+};
+
+BeerSong.prototype.bottleString = function (bottleCount) {
+ if (bottleCount === 1) return 'it';
+ return 'one';
+};
+
+BeerSong.prototype.leftPart = function(bottles) {
+ let str = this.countPhrase(bottles) + ' of beer on the wall, ' + this.countPhrase(bottles) + ' of beer.';
+ return str.slice(0,1).toUpperCase() + str.slice(1); 
+}
+
+BeerSong.prototype.rightPart = function(bottles) {
+ if (bottles === 0) return 'Go to the store and buy some more, 99 bottles of beer on the wall.\n';
+ return 'Take ' + this.bottleString(bottles) + ' down and pass it around, ' + this.countPhrase(bottles - 1) + ' of beer on the wall.\n';
+}
+
 BeerSong.prototype.sing = function(start, end) {
-    if(!end) end = 0;
-    if(isNaN(start) || isNaN(end)) throw Error('invalid input!');
-    let result = [];
-    for(var i = start; i >= end; i--) {
-		result.push(this.verse(i));
-	}
-	return result.join('\n');
+ if(!end) end = 0;
+ if(isNaN(start) || isNaN(end)) throw Error('invalid input!');
+ let result = [];
+ for(var i = start; i >= end; i--) {
+  result.push(this.verse(i));
+ }
+ return result.join('\n');
 };
 
 module.exports = BeerSong;
